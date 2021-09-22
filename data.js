9e67690259192780810e90036bf9e3525058df9e
CodeCinteriors <91065284+CodeCinteriors@users.noreply.github.com>
2021-09-22 17:00:09 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-mbb0001",
+      "name": "MBB0001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.29267206182279715,
+          "pitch": 0.05236275275496638,
+          "rotation": 0,
+          "target": "1-mba0001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-mba0001",
+      "name": "MBA0001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1733547747195576,
+          "pitch": 0.059844470625964874,
+          "rotation": 0,
+          "target": "0-mbb0001"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Master Bedroom-AK Block",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
